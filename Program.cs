053ee616dacabcdb5044cd0c85c1633e79ef9a81
lovely suma <sumalatha11@gmail.com>
2023-07-25 16:14:22 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿ using System;
+ using System.IO;
+
+namespace StudentDataRetrieval
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Path to the text file
+            string filePath = "D:Student_Data.txt";
+
+            // Check if the file exists
+            if (File.Exists(filePath))
+            {
+                // Read all lines from the file
+                string[] lines = File.ReadAllLines(filePath);
+
+                // Display the student data
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Student data file not found!");
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+
+}
+
+
